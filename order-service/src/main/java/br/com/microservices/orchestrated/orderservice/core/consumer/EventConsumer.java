5fab3c276720913894d3fdf02e5ec4165b7d5625
Bruno Murakami <murakami.bruno@gmail.com>
2024-01-08 20:54:22 -0300

    feat(order-service) Criação dos services e ajuste do EventConsumer

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/consumer/EventConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.orderservice.core.consumer;
 
+import br.com.microservices.orchestrated.orderservice.core.service.EventService;
 import br.com.microservices.orchestrated.orderservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 @AllArgsConstructor
 public class EventConsumer {
 
+    private final EventService service;
     private final JsonUtil jsonUtil;
     @KafkaListener(
         groupId = "${spring.kafka.consumer.group-id}",
@@ -19,7 +21,7 @@ public class EventConsumer {
     public void consumeNotifyEndingEvent(String payload) {
         log.info("Receiving ending notification event from {} from notify-ending topic", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        service.notifyEnding(event);
     }
 
 }
